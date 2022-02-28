4833f3fb52faef42500936d7a57d8e94433032c7
Vivaan Chakma <100195681+vivaanchakma@users.noreply.github.com>
2022-02-28 20:34:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
+
+let errorParagraph = document.getElementById("error")
+
+
+function purchase(){
+    errorParagraph.textContent = "Something went wrong, please try again"
+}
+
+console.log("Vivaan Chakma 28/02/2022")
+
+
